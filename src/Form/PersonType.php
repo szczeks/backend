b381394091d5cb0eccd1b04c3c2f793f5811b2ee
szczeks <pszczepanek@protonmail.ch>
2021-01-03 20:13:06 +0100

    twig templates

---
+++ src/Form/PersonType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Person;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('login')
+            ->add('lName')
+            ->add('fName')
+            ->add('state')
+            ->add('product')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Person::class,
+        ]);
+    }
+}
